e243c828671db6c9db4c1114aeb8ebad8bed7e29
0Fly98 <lundujosh97@gmail.com>
2024-08-09 06:07:15 -0700

    game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,23 +3,37 @@
 """
 
 
-def island_perimeter(grid):
-    """Computes the perimeter of an island with no lakes.
-    """
-    perimeter = 0
-    if type(grid) != list:
-        return 0
-    n = len(grid)
-    for i, row in enumerate(grid):
-        m = len(row)
-        for j, cell in enumerate(row):
-            if cell == 0:
-                continue
-            edges = (
-                i == 0 or (len(grid[i - 1]) > j and grid[i - 1][j] == 0),
-                j == m - 1 or (m > j + 1 and row[j + 1] == 0),
-                i == n - 1 or (len(grid[i + 1]) > j and grid[i + 1][j] == 0),
-                j == 0 or row[j - 1] == 0,
-            )
-            perimeter += sum(edges)
-    return perimeter
+def isWinner(x, nums):
+    def sieve_of_eratosthenes(limit):
+        primes = [True] * (limit + 1)
+        primes[0], primes[1] = False, False
+
+        for num in range(2, int(limit**0.5) + 1):
+            if primes[num]:
+                for multiple in range(num*num, limit + 1, num):
+                    primes[multiple] = False
+
+        return [num for num, is_prime in enumerate(primes) if is_prime]
+
+    def can_make_move(num, primes):
+        for prime in primes:
+            if prime > num:
+                break
+            if num % prime == 0:
+                return True
+        return False
+
+    wins = {'Maria': 0, 'Ben': 0}
+    max_num = max(nums)
+    all_primes = sieve_of_eratosthenes(max_num)
+
+    for n in nums:
+        if can_make_move(n, all_primes):
+            wins['Maria' if x % 2 == 1 else 'Ben'] += 1
+
+    if wins['Maria'] > wins['Ben']:
+        return 'Maria'
+    elif wins['Ben'] > wins['Maria']:
+        return 'Ben'
+    else:
+        return Non
